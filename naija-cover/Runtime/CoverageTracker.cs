081d31968a03279d17c46fe66b5155e1d17bba2c
bolorundurowb <ogatimo@gmail.com>
2017-01-13 20:58:22 +0100

    feature: adds generate lcov function

--- naija-cover/Runtime/CoverageTracker.cs
+++ naija-cover/Runtime/CoverageTracker.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace naija_cover.Runtime
 {
@@ -7,6 +8,21 @@ namespace naija_cover.Runtime
         private static Dictionary<string, Dictionary<int, bool>> coverage = new Dictionary<string, Dictionary<int, bool>>();
 
         public static void WriteCoverageToFile() {}
+
+        public static string GenerateLcov()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string fileName in coverage.Keys)
+            {
+                sb.Append("SF:" + fileName + "\n");
+                foreach (KeyValuePair<int,bool> line in coverage[fileName])
+                {
+                    sb.Append(string.Format("DA:{0},{1}\n", line.Key, line.Value ? 1 : 0));
+                }
+                sb.Append("end_of_record\n");
+            }
+            return sb.ToString();
+        }
 
         public static void MarkExecuted(string fileName, int line)
         {
