fb4ee19ad70a3a89c469441a28ed5da673106277
bolorundurowb <ogatimo@gmail.com>
2017-01-13 16:49:38 +0100

    feature: adds coverage tracker class

---
+++ naija-cover/CoverageTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace naija_cover
+{
+    public class CoverageTracker
+    {
+        private static Dictionary<string, Dictionary<int, bool>> coverage = new Dictionary<string, Dictionary<int, bool>>();
+
+        public static void WriteCoverageToFile() {}
+
+        public static void MarkExecuted(string filName, int line)
+        {
+            if (!coverage.ContainsKey(filName))
+            {
+                coverage.Add(filName, new Dictionary<int, bool>());
+            }
+            coverage[filName].Add(line, true);
+        }
+    }
+}
