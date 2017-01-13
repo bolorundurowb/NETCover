435f864629f32b8a387672216229a569f3002949
bolorundurowb <ogatimo@gmail.com>
2017-01-13 16:19:36 +0100

    chore: add project scaffold

---
+++ naija-cover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace naija_cover
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
