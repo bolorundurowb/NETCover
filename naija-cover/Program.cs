a897166eb6b6905e3dd57bd2dd7086ca21414e7d
bolorundurowb <ogatimo@gmail.com>
2017-01-16 14:29:02 +0100

    chore: readds the nuget packages

--- naija-cover/Program.cs
+++ naija-cover/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using naija_cover.Runtime;
 
 namespace naija_cover
@@ -7,8 +9,12 @@ namespace naija_cover
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Naija Cover 1.0.0");
             //
+            List<string> arguments = args.ToList();
+            int directoryFlag = arguments.IndexOf("-d");
+
+            // Make sure coverage is written before closing
             AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => CoverageTracker.WriteCoverageToFile();
         }
     }
