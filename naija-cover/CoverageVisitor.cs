ad52fa3d93f6e75fb2174a1ef3b265ec1c58d76d
bolorundurowb <ogatimo@gmail.com>
2017-01-17 12:36:51 +0100

    fix: removed all packages and used monodevelop nuget to install them

--- naija-cover/CoverageVisitor.cs
+++ naija-cover/CoverageVisitor.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using naija_cover.Runtime;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace naija_cover
 {
@@ -15,15 +15,37 @@ namespace naija_cover
             fileName = filename;
         }
 
-        private void MakeCoverageTrackingCall(TypeDeclarationSyntax node)
+        private SyntaxNode MakeCoverageTrackingCall(TypeDeclarationSyntax node)
         {
             MakeCoverageTrackingCall(node, true);
         }
 
-        private void MakeCoverageTrackingCall(TypeDeclarationSyntax node, bool passInNode)
+        private SyntaxNode MakeCoverageTrackingCall(TypeDeclarationSyntax node, bool passInNode)
         {
             var lineNumber = node.GetLocation().GetLineSpan().StartLinePosition.Line;
             CoverageTracker.MarkExecutable(fileName, lineNumber);
+            //
+            var method = MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression,
+                MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression,
+                    MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression,
+                        IdentifierName("naija_cover"), IdentifierName("Runtime")),
+                    IdentifierName("CoverageTracker")),
+                IdentifierName("MarkExecuted"));
+            var filePathArg = Argument(LiteralExpression(SyntaxKind.StringLiteralExpression, Literal(fileName)));
+            var lineNumberArg = Argument(LiteralExpression(SyntaxKind.NumericLiteralExpression, Literal(lineNumber)));
+            SeparatedSyntaxList<ArgumentSyntax> argumentList;
+            if (passInNode)
+            {
+                var nodeArg = Argument(LiteralExpression())
+                argumentList = SeparatedList(new[] {filePathArg, lineNumberArg, nodeArg});
+            }
+            else
+            {
+                argumentList = SeparatedList(new[] {filePathArg, lineNumberArg});
+            }
+            var methodCallExpr = ExpressionStatement(
+                InvocationExpression(method, ArgumentList(argumentList)));
+            return methodCallExpr;
         }
     }
 }
