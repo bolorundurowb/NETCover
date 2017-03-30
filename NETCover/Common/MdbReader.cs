68ef540ed42d6f01030141e95d90f5d4d0599fb2
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-03-30 23:05:04 +0100

    feature: adds the mdbReader scaffold

---
+++ NETCover/Common/MdbReader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Mono.Cecil;
+
+namespace NETCover.Common
+{
+    public class MdbReader : IProgramDatabaseReader
+    {
+        public void Initialize(string assemblyFilePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDictionary<uint, CodeSegment> GetSegmentsByMethod(MethodDefinition methodDef)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
